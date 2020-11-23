c245a6fafae9a3bf8b2ad87a583e38b1d577bc53
jakeintravaia <46909741+jakeintravaia@users.noreply.github.com>
2020-11-23 16:46:21 -0500

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 function checkLogin(){
   $username = $_POST['username'];
   $password = $_POST['password'];
-  if($username != "admin" || $password != "password"){
+  if(md5($username) != "21232f297a57a5a743894a0e4a801fc3" || md5($password) != "5f4dcc3b5aa765d61d8327deb882cf99"){
     return false;
   }
   return true;
